5fc112caf82398f18c8edc6633a73766a5da6c19
bbk <2087108700@qq.com>
2021-07-11 03:59:35 +0800

    🎈 perf(公共组件全局引入后优化代码): 全局引入公共组件其他页面直接使用

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,9 +1,11 @@
-import Vue from 'vue'
-import SvgIcon from '@/components/SvgIcon'// svg component
-
-// register globally
-Vue.component('svg-icon', SvgIcon)
-
+/*
+ * @Author       : Jason <2087108700@qq.com>
+ * @Date         : 2021-07-09 23:49:57
+ * @LastEditTime : 2021-07-11 03:51:05
+ * @LastEditors  : Jason
+ * @Description  : 载入svg图片
+ * @FilePath     : \vue-admin-template-plus\src\icons\index.js
+ */
 const req = require.context('./svg', false, /\.svg$/)
 const requireAll = requireContext => requireContext.keys().map(requireContext)
 requireAll(req)
--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -33,14 +33,8 @@
 
 <script>
 import { mapGetters } from 'vuex'
-import Breadcrumb from '@/components/Breadcrumb'
-import Hamburger from '@/components/Hamburger'
 
 export default {
-  components: {
-    Breadcrumb,
-    Hamburger
-  },
   computed: {
     ...mapGetters([
       'sidebar',
